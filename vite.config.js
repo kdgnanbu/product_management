2d52cce8623ea740fcb28887c208cf5cfc9801f1
南部椋二 <nanbumukuni@nanbumukunis-MacBook-Pro.local>
2026-01-01 21:46:16 +0900

    set base path for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  base: '/product_management/' // ← リポジトリ名
+  plugins: [vue(), vueDevTools()],
+  base: '/product-management/', // ← リポジトリ名（※ - ）
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
